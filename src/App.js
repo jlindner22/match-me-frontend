fe74ba0d60e6fe33ee2bf6c51fe0d1bc02f212cb
jlindner22 <jenlindner22@gmail.com>
2020-06-12 13:09:29 -0400

    added .flaskenv file, changed proxy, edited App.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+// class App extends React.Component {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  // render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> The current time is {currentTime}.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +29,12 @@ function App() {
         >
           Learn React
         </a>
+          HI
       </header>
     </div>
   );
+// }
 }
+
 
 export default App;
